873d0c0f36a1f88a87759dd2f35398bf20f6f858
haonx <qtvhao@gmail.com>
2017-07-15 20:48:41 +0700

    create Task extends Model that interactive with database table 'tasks'

---
+++ app/Task.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: admin
+ * Date: 7/15/2017
+ * Time: 8:47 PM
+ */
+
+namespace App;
+
+
+use Illuminate\Database\Eloquent\Model;
+
+class Task extends Model
+{
+
+}
