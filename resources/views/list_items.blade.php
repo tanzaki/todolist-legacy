784ee9f5eec0cca94c8b15a65541b795eadcd3ba
haonx <qtvhao@gmail.com>
2017-07-18 18:16:04 +0700

    update phpDoc for auto-suggest on IDE

--- resources/views/list_items.blade.php
+++ resources/views/list_items.blade.php
@@ -2,9 +2,11 @@
 @section('content')
     <div>
         <?php
+        use App\Task;
         use Illuminate\Database\Eloquent\Collection;
         /** @var Collection|array $tasksItems */
         //TasksItems is special Object, We can foreach it as array
+        /** @var Task $taskItem */
         foreach ($tasksItems as $taskItem) {
             echo "<div>$taskItem</div>";
         }
